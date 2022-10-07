c87268c925759684042e560e42a86f9672e03480
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-07 23:06:58 +0200

    Create necessary models and controllers
    Add devise to the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+
+  resources :posts, only: [:new, :create, :update]
 end
