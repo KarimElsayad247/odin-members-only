3037254bbdac43a467b08e369d96b07a6f4d9937
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-08 12:21:30 +0200

    Fill out posts controller
    Login checking is not implemented yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,31 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def require_login
+    true
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
